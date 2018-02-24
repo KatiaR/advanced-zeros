3d6baf4efe3acdb8b4a4badc63caee84b137f043
Katia Romaniuk <katia-l@mail.ru>
2018-02-24 15:55:49 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let result = number;
+    let findMiddle = base;
+    for (let i = 2; i <= base; i++) {
+        if (!(findMiddle % i)) {
+            let middle = 0;
+            let count = 0;
+            let numbers = number;
+            while (!(findMiddle % i)) {
+                middle++;
+                findMiddle = Math.floor(findMiddle / i);
+            }
+            while (numbers / i > 0) {
+                count += Math.floor(numbers / i);
+                numbers = Math.floor(numbers / i);
+            }
+            result = Math.min(result, Math.floor(count / middle));
+        }
+    }
+    return result;
+};
